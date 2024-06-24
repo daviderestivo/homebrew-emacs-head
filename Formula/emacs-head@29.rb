b0293851151533150a254a460230444d4bc65ddd
Davide Restivo <davide.restivo@yahoo.it>
2024-06-24 10:30:26 +0200

    Bump emacs-29 Formula to 29.4

--- Formula/emacs-head@29.rb
+++ Formula/emacs-head@29.rb
@@ -2,10 +2,10 @@
 require_relative "../Library/EmacsBase"
 
 class EmacsHeadAT29 < EmacsBase
-  url "https://ftp.gnu.org/gnu/emacs/emacs-29.3.tar.xz"
-  mirror "https://ftpmirror.gnu.org/emacs/emacs-29.3.tar.xz"
-  sha256 "c34c05d3ace666ed9c7f7a0faf070fea3217ff1910d004499bd5453233d742a0"
-  version "29.3"
+  url "https://ftp.gnu.org/gnu/emacs/emacs-29.4.tar.xz"
+  mirror "https://ftpmirror.gnu.org/emacs/emacs-29.4.tar.xz"
+  sha256 "ba897946f94c36600a7e7bb3501d27aa4112d791bfe1445c61ed28550daca235"
+  version "29.4"
   revision 1
 
   head do
